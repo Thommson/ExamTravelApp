bb8e16615e67a554a4ac460d9038b59fe28fc078
Maria Aldis <maria.aldisg@gmail.com>
2019-01-05 18:57:06 +0100

    added icons for activites+nav

--- src/components/ui/TripCard.js
+++ src/components/ui/TripCard.js
@@ -34,7 +34,16 @@ class TripCard extends Component {
             </div>
 
             <div className="col">
-                <button className="buttons">Update</button>
+                <button className="buttons">
+                <div class="update">Update</div>
+                <svg id='Layer_1' class="svgButton" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <path d='M3,17.5v3C3,20.8,3.2,21,3.5,21h3c0.1,0,0.3,0,0.3-0.1L17.8,9.9l-3.8-3.8L3.2,17.1C3.1,17.2,3,17.3,3,17.5z M20.7,7	c0.4-0.4,0.4-1,0-1.4l-2.3-2.3c-0.4-0.4-1-0.4-1.4,0l-1.8,1.8l3.8,3.8C18.9,8.9,20.7,7,20.7,7z'
+                    />
+                </svg>
+                
+                </button>
+
+                
             </div>
         </div>
     </div>
